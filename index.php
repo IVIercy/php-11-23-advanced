9ebf8b5ae420e74f93b06561ee97b5427198203f
Vladyslav Parkhomenko <i1ame050598@gmail.com>
2023-11-22 19:38:13 +0200

    '[hw_2]'

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-phpinfo();
+
+require_once __DIR__ . '/Color.php';
+
+$color = new Color(45, 70, 56);
+
+$newColor = $color->randColor();
+
+$equals = $newColor->equals($newColor) . PHP_EOL;
+
+$mixedColor = $color->mix($newColor);
+
+echo $mixedColor->getRed() . PHP_EOL;
+echo $mixedColor->getGreen() . PHP_EOL;
+echo $mixedColor->getBlue() . PHP_EOL;
+
